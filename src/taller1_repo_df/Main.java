2f6acd4e1ea83489a3b3a93cf3b8a778f8f2cdd8
David Fueltala <David Fueltala@LAPTOP-MVFCKI5V>
2022-05-18 09:38:36 -0500

    Archivos necesarios para el proyecto

---
+++ src/taller1_repo_df/Main.java
@@ -0,0 +1,12 @@
+package taller1_repo_df;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Mi primer proyecto JAVA en GitHub");
+
+	}
+
+}
